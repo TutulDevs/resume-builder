23251bdb77b42cd69dc06e8e9513a4e9af8e9d42
Tutul <tutulnahid@gmail.com>
2021-07-09 20:45:05 +0600

    added form inputs & passing data

--- src/components/Form.js/Form.js
+++ src/components/Form.js/Form.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
 
-const schema = yup.object().shape({
-  firstName: yup.string().required(),
-  age: yup.number().min(4).max(8).positive().integer().required(),
-});
+// const schema = yup.object().shape({
+//   firstName: yup.string().required(),
+//   age: yup.number().min(4).max(8).positive().integer().required(),
+// });
 
 const Form = (props) => {
+  const [showSocial, setShowSocial] = useState(false);
+  const [showSkills, setShowSkills] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -19,27 +22,147 @@ const Form = (props) => {
   const onSubmit = (data) => props.onSubmitForm(data);
 
   const inputIsValid =
-    "py-1 px-2 w-full bg-white border border-header rounded-lg focus:ring focus:ring-blue-300 focus:outline-none";
+    "py-1 px-2 w-full bg-white border border-blue-200 rounded-lg focus:ring focus:ring-blue-300 focus:outline-none";
   const inputIsInvalid =
     "py-1 px-2 w-full bg-white border border-red-300 rounded-lg focus:ring focus:ring-red-300 focus:outline-none";
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className='my-2'>
-        <label htmlFor='firstName'>First Name</label>
-        <input
-          name='firstName'
-          id='firstName'
-          type='text'
-          {...register("firstName", { required: true })}
-          className={errors.firstName ? inputIsInvalid : inputIsValid}
-        />
+      {/* Header elements */}
+      <h2 className='border-b border-header text-header text-xl font-medium mt-4'>
+        Basic Information
+      </h2>
+      <div className='basicInfo'>
+        <div className='my-2'>
+          <label htmlFor='fullName'>First Name</label>
+          <input
+            name='fullName'
+            id='fullName'
+            type='text'
+            {...register("fullName", { required: true })}
+            className={errors.fullName ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+
+        <div className='my-2'>
+          <label htmlFor='jobTitle'>Job Title</label>
+          <input
+            name='jobTitle'
+            id='jobTitle'
+            type='text'
+            {...register("jobTitle", { required: true })}
+            className={errors.jobTitle ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+      </div>
+
+      {/* Socials */}
+      <h2
+        onClick={() => setShowSocial(!showSocial)}
+        className='border-b border-header text-header text-xl font-medium mt-4'>
+        Social Handlers {showSocial ? "+" : "-"}
+      </h2>
+
+      <div
+        className={`${
+          showSocial && "hidden"
+        } socials grid gap-2 sm:grid-flow-col`}>
+        <div className='my-2'>
+          <label htmlFor='email'>Email</label>
+          <input
+            name='email'
+            id='email'
+            type='email'
+            {...register("email", { required: true })}
+            className={errors.email ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+
+        <div className='my-2'>
+          <label htmlFor='linkedIn'>LinkedIn</label>
+          <input
+            name='linkedIn'
+            id='linkedIn'
+            type='text'
+            placeholder='e.g. tutuldevs'
+            {...register("linkedIn", { required: true })}
+            className={errors.linkedIn ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+
+        <div className='my-2'>
+          <label htmlFor='github'>GitHub</label>
+          <input
+            name='github'
+            id='github'
+            type='text'
+            placeholder='e.g. tutuldevs'
+            {...register("github", { required: true })}
+            className={errors.github ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+
+        <div className='my-2'>
+          <label htmlFor='portfolio'>Portfolio Site</label>
+          <input
+            name='portfolio'
+            id='portfolio'
+            type='url'
+            placeholder='Optional'
+            {...register("portfolio")}
+            className={errors.portfolio ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+      </div>
+
+      <h2
+        onClick={() => setShowSkills(!showSkills)}
+        className='border-b border-header text-header text-xl font-medium mt-4'>
+        Skills {showSkills ? "+" : "-"}
+      </h2>
+
+      <div className={`${showSkills && "hidden"} skills`}>
+        <div className='my-2'>
+          <label htmlFor='specializations'>Specializations</label>
+          <input
+            name='specializations'
+            id='specializations'
+            type='text'
+            placeholder='e.g. Single Page Application, Front-End Development, RWD'
+            {...register("specializations", { required: true })}
+            className={errors.specializations ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+
+        <div className='my-2'>
+          <label htmlFor='development'>Development</label>
+          <input
+            name='development'
+            id='development'
+            type='text'
+            placeholder='e.g. HTML, CSS, SASS, JavaScript, React, Redux'
+            {...register("development", { required: true })}
+            className={errors.development ? inputIsInvalid : inputIsValid}
+          />
+        </div>
+
+        <div className='my-2'>
+          <label htmlFor='tools'>Tools</label>
+          <input
+            name='tools'
+            id='tools'
+            type='text'
+            placeholder='e.g. VS Code, Git, GitHub, ZenHub, Netlify, Firebase'
+            {...register("tools")}
+            className={errors.tools ? inputIsInvalid : inputIsValid}
+          />
+        </div>
       </div>
 
       <div className='my-4'>
         <button
           type='submit'
-          className='py-2 px-4 bg-header text-white rounded-lg transition duration-100 focus:ring-4 focus:ring-blue-400'>
+          className='py-2 px-6 bg-header text-white rounded-lg transition duration-100 focus:ring-4 focus:ring-blue-400'>
           Submit
         </button>
       </div>
@@ -48,23 +171,3 @@ const Form = (props) => {
 };
 
 export default Form;
-
-/*
-  const Input = ({ type, id, label, register, required, minLength }) => {
-    return (
-      <div className='mt-2'>
-        <label htmlFor={id} className='block'>
-          {label}
-        </label>
-
-        <input
-          type={type}
-          id={id}
-          {...register(id, { required, minLength })}
-          className='py-1 px-2 w-full bg-white border border-header rounded-lg focus:ring focus:ring-blue-300 focus:outline-none'
-        />
-      </div>
-    );
-  };
-
-*/
---
+++ src/components/UI/Icons.js
@@ -0,0 +1,35 @@
+export const UpIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-3 w-3'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M5 11l7-7 7 7M5 19l7-7 7 7'
+      />
+    </svg>
+  );
+};
+
+export const DownIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-3 w-3'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M19 13l-7 7-7-7m14-8l-7 7-7-7'
+      />
+    </svg>
+  );
+};
